b93506de08f10bfea1919f01d7f3750828f9b807
avikamutia6 <151595347+avikamutia6@users.noreply.github.com>
2024-05-01 10:32:30 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+// Toggle class active
+const navbarNav = document.querySelector(".navbar-nav");
+//Ketika Hamburger menu di klik
+document.querySelector("#hamburger-menu").onclick = () => {
+  navbarNav.classList.toggle("active");
+};
+// Klik di luar sidebar untuk menghilangkan nav
+const Hamburger = document.querySelector("#hamburger-menu");
+document.addEventListener("click", function (e) {
+  if (!Hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
+    navbarNav.classList.remove("active");
+  }
+});
